b69bc5fa03669bdaca1ef58beb6e75b57b91a332
Ciaran Hikaru Ueda Fitzgerald <chuf@dhcp-wifi-8021x-155-41-20-130.bu.edu>
2021-12-12 13:58:53 -0500

    Fully removed 'report' app and renamed 'song-title' page to 'report'

--- prototype/form/urls.py
+++ prototype/form/urls.py
@@ -4,5 +4,5 @@ from form import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('form/', views.form, name='form'),
-    path('form/song_title/', views.song_title,name='')
+    path('report/', views.report,name='report')
 ]
--- prototype/prototype/urls.py
+++ prototype/prototype/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('form.urls')),
-    path('', include('report.urls')),
     path('social/', include('social_django.urls')),
     path('', include('django.contrib.auth.urls'))
 ]
