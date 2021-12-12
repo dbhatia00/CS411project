7134560ab420e70616ce1b09dd9102f6b32b18af
Ciaran Hikaru Ueda Fitzgerald <chuf@dhcp-wifi-8021x-155-41-20-130.bu.edu>
2021-12-12 13:53:50 -0500

    removed 'report' app

--- prototype/prototype/settings.py
+++ prototype/prototype/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'form',
-    'report',
     'social_django'
 ]
 
--- prototype/report/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from report import views
-
-urlpatterns = [
-    path('report/', views.report, name='report'),
-]
